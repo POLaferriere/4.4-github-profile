c88314779b147082ba118aa816489b56d3b441c8
Pierre Laferriere <polaferriere@gmail.com>
2015-09-17 17:32:15 -0400

    Header completed with jquery profile picture

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
-//AJAX request to get all repository objects, logs the objects
-$.ajax({
+var user = {
   url: 'https://api.github.com/users/POLaferriere',
   method: "GET",
   headers: {"Authorization": "token 21d92ccad43c59f3681a32a26b66a41f04711abd"}
-}).then(function(whatever){
+};
+
+var repo = {
+  url: 'https://api.github.com/user/repos',
+  method: "GET",
+  headers: {"Authorization": "token 21d92ccad43c59f3681a32a26b66a41f04711abd"}
+};
+
+//AJAX request to get all repository objects, logs the objects
+$.ajax(user).then(function(whatever){
+  console.log(whatever);
+  var newThumbnail = whatever.avatar_url;
+  $('.profile-thumbnail').attr('src', newThumbnail);
+});
+
+$.ajax(repo).then(function(whatever){
   console.log(whatever);
 });
